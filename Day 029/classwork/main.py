b84e582d34fe72561b8a49c3219734fc70ae7bc9
bgharashvilimarii <mariambgharashvili@gmail.com>
2026-01-21 01:03:23 +0400

    second commit

---
+++ Day 029/classwork/main.py
@@ -0,0 +1,49 @@
+nums = [1,2,3,4,5]
+print(nums[0])
+
+num = [1,2,3,4,5]
+print(num[-1])
+
+nums = [1,2,3,4,5]
+print(nums[:3])
+
+num = [1,2,3,4,5]
+print(num[-2:])
+
+
+number = [1,2,3,4,5]
+print(number[1:4])
+
+n = [1,2,3,4,5]
+print(n[::-1])
+
+i = [1,2,3,4,5]
+print(i[1])
+
+numb = [1,2,3,4,5]
+print(numb[::2])
+
+# N9
+
+list = [1,2,3,4,5]
+list1 = len(list) // 2
+print(list[list1])
+
+# N10
+
+list = (1,2,3,4,5,6)
+print(list[:len(list)//2])
+
+
+
+# n9
+list = [1,2,3,4,5]
+print(list[2])
+
+# n10
+list = [1,2,3,4,5,6]
+print(list[:3])
+
+
+
+
