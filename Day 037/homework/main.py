49d3c11c69b004f85a3543748b2048501716583d
bgharashvilimarii <mariambgharashvili@gmail.com>
2026-02-15 18:42:23 +0400

    second commit

---
+++ Day 037/homework/main.py
@@ -0,0 +1,17 @@
+# default პარამეტრი aris iseti funqciis parametri romelsac winaswar gansazgvruli mnishvneloba akvs.
+# igi am mnishvnelobas mashin ikenebs rodesac argumentad am parametrs araferi gadaecema, amitom igi mas avtomaturad mianichebs
+# im mnishvnelobas rasac chven gavutolebt for example: x = 5 am shemtxvevashi x-is default mnishveloba 5-ia
+
+#  შექმენით ფუნქცია, რომელსაც გადაეცემა default პარამეტრი სახელად name.
+#  for ციკლის მეშვეობით გადაუარეთ გადმოცემულ name პარამეტრს და დაბეჭდეთ სახელის თითოეული სიმბოლო.
+
+def name(x="mari"):
+    for i in x:
+        print(i)
+name()
+
+# rodesac default parametrsac gadascem da argumentsac uwer
+def name(x="mari"):
+    for i in x:
+        print(i)
+name("dachi")
