0f0b937f00b39677a815f0ce2d6c90ff4846a50b
bgharashvilimarii <mariambgharashvili@gmail.com>
2026-05-18 21:02:40 +0400

    second commit

---
+++ Level 062/classwork/script.js
@@ -0,0 +1,17 @@
+// number -zogadad yvela tipis ricxvs moiazrebs 5 , 5.0 , 5.07897 -3 da a.sh
+// bigint - aris ricxvi 2 ** 53 zevit 
+// string- kvelaferi rac ""-shi weria "hello" , "5"
+// Boolean true and false
+// null 0
+// undefined - igive amoucnobi
+// Symbol yvela simbolo mag %$& da a.sh
+
+console.log(5 + 5);
+console.log(5 * 5);
+console.log(5 - 5);
+console.log(5 / 5);
+
+// console aris igive pytonis print anu gamoyeneba imistvis rom chven shedegi davinaxot
+console.log("Hello " + "World");
+console.log("Hello" + "5");
+console.log("5" + "5");
---
+++ Level 062/homework/script.js
@@ -0,0 +1,30 @@
+/* 1) ახსენით რას ნიშნავს ის რომ ჯავასკრიპტი იყო სკრიპტინგის ენა და რის შემდეგ გახდა პროგრამირების ენა
+2) რითი განსხვავდება undefined, null ერთმანეთისგან
+3) რას აკეთებს .length კუთვნილება? გააკეთეთ 3 მაგალითი
+4) რას აკეთებს .toUpperCase()? გააკეთეთ 3 მაგალითი */
+
+
+
+
+// 1)javascript tavidan sheiqmna rogorc scriptingis ena rac imas nishnavda rom igi gamoikeneboda patar-patara davalebebis shesasruleblad
+
+// მოგვიანებით Google-მა შექმნა სწრაფი JavaScript ძრავი V8, ხოლო შემდეგ გამოჩნდა Node.js, რომელმაც JavaScript-ს ბრაუზერის გარეთ 
+// მუშაობის შესაძლებლობა მისცა. ამის შემდეგ უკვე შესაძლებელი გახდა:
+// სერვერების შექმნა
+// მონაცემთა ბაზებთან მუშაობა
+// აპლიკაციების შექმნა
+// თამაშებისა და დიდი სისტემების განვითარება
+// ამ ეტაპიდან JavaScript აღარ ითვლებოდა
+//  მხოლოდ სკრიპტინგის ენად და გადაიქცა სრულფასოვან პროგრამირების ენად, 
+// რადგან მას შეეძლო დიდი და დამოუკიდებელი პროგრამების შექმნაც.
+
+//2) undefined aris rodesac monacemisatvis gadacemuli mnishvneloba ar arsebobs xolo null aris rodesac monacemi ar arsebobs
+// 3).length itvlis characterebis raodenobas  (igivea rac len funqcia pythonshi)
+console.log("mari".length);
+console.log("HelloWorld".length);
+console.log("GOAL ORIENTED ACADEMY".length);
+
+// toUpperCase() gamoikeneba imistvis rom gadacemuli stringi gadaaikvanos uppercase-shi anu didi asoebit daweros (igivea rac .upper() pythonshi)
+console.log("GOAL ORIENTED ACADEMY".toUpperCase());
+console.log("HelloWorld".toUpperCase());
+console.log("mari".toUpperCase());
